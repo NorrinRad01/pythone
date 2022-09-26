ac03ebf758153516c8f1aafe87df3dc13600b08a
NorrinRadd <Epicever94@gmail.com>
2022-09-26 21:38:26 +0300

    homework1

---
+++ Homework1/task5.py
@@ -0,0 +1,26 @@
+def inputNumbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"координата по оси {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Введите целые числа!")
+    return a
+
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = inputNumbers(2)
+print("Введите координаты точки В")
+pointB = inputNumbers(2)
+
+print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
